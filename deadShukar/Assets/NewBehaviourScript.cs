757bea4197070514159ec0cb1f5403f9ef49cc08
Student3 <Student3@DESKTOP-L84RCKF>
2023-12-03 14:00:08 +0300

    added project

--- deadShukar/Assets/NewBehaviourScript.cs
+++ deadShukar/Assets/NewBehaviourScript.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 5f;
+    // Start is called before the first frame
     void Update()
     {
-        
+        float moveHorizontal =
+            Input.GetAxis("Horizontal");
+
+        // вправо и влево
+        Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
+        transform.position += movement * speed * Time.deltaTime;
     }
 }
+  
---
+++ deadShukar/Assets/comebak.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BioatController : MonoBehaviour
+{
+    public SpriteRenderer
+        spriteRenderer;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetkeyDown(KeyCode.LeftArrow))
+    }
+}
+
+  
